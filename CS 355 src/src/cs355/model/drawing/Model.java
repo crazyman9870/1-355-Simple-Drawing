f568755fcca18bf691427dfecd51dd4559fcede8
Anthony Constantino <aconstantino@neovest.com>
2016-01-07 15:06:48 -0700

    Beginning changes

--- CS 355 src/src/cs355/model/drawing/Model.java
+++ CS 355 src/src/cs355/model/drawing/Model.java
@@ -1,12 +1,19 @@
 package cs355.model.drawing;
 
 import java.awt.Color;
+import java.util.ArrayList;
 
 public class Model extends Shape {
 
+	ArrayList<Shape> shapes;
+	
 	public Model(Color color) {
 		super(color);
 		// TODO Auto-generated constructor stub
+	}
+	
+	public ArrayList<Shape> getShapes() {
+		return shapes;
 	}
 
 }
--- CS 355 src/src/cs355/view/View.java
+++ CS 355 src/src/cs355/view/View.java
@@ -3,17 +3,17 @@ package cs355.view;
 import java.awt.Graphics2D;
 import java.util.Observable;
 
+import cs355.GUIFunctions;
+
 public class View implements ViewRefresher {
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
-		// TODO Auto-generated method stub
-
+		GUIFunctions.refresh();
 	}
 
 	@Override
 	public void refreshView(Graphics2D g2d) {
-		// TODO Auto-generated method stub
-
+		//ArrayList<Shape> shapes = 
 	}
 }
