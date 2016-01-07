b6d46fd5f7708dc351ae23d93251c7af50837948
Anthony Constantino <aconstantino@neovest.com>
2016-01-07 14:30:31 -0700

    MVC Classes created
    These should be the classes needed to complete the project

---
+++ CS 355 src/src/cs355/model/drawing/Model.java
@@ -0,0 +1,12 @@
+package cs355.model.drawing;
+
+import java.awt.Color;
+
+public class Model extends Shape {
+
+	public Model(Color color) {
+		super(color);
+		// TODO Auto-generated constructor stub
+	}
+
+}
