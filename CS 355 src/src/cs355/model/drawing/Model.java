b264e85c2f15e24ad58c46bf0c799b85b09dad1c
crazyman9870 <crazyman9870@gmail.com>
2016-01-07 17:11:42 -0700

    File Changes on Learning Suite

--- CS 355 src/src/cs355/model/drawing/Model.java
+++ CS 355 src/src/cs355/model/drawing/Model.java
@@ -2,18 +2,76 @@ package cs355.model.drawing;
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.List;
 
-public class Model extends Shape {
+public class Model extends CS355Drawing {
 
-	ArrayList<Shape> shapes;
-	
-	public Model(Color color) {
-		super(color);
-		// TODO Auto-generated constructor stub
+	private static Model _instance;
+
+	public static Model instance() {
+		if (_instance == null) 
+			_instance = new Model();
+		return _instance;
 	}
 	
-	public ArrayList<Shape> getShapes() {
-		return shapes;
+	@Override
+	public Shape getShape(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int addShape(Shape s) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void deleteShape(int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveToFront(int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void movetoBack(int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveForward(int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveBackward(int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Shape> getShapes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Shape> getShapesReversed() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setShapes(List<Shape> shapes) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- CS 355 src/src/cs355/view/View.java
+++ CS 355 src/src/cs355/view/View.java
@@ -1,9 +1,13 @@
 package cs355.view;
 
 import java.awt.Graphics2D;
+import java.util.ArrayList;
 import java.util.Observable;
 
 import cs355.GUIFunctions;
+import cs355.model.drawing.Model;
+import cs355.model.drawing.Shape;
+import cs355.model.scene.Instance;
 
 public class View implements ViewRefresher {
 
@@ -14,6 +18,10 @@ public class View implements ViewRefresher {
 
 	@Override
 	public void refreshView(Graphics2D g2d) {
-		//ArrayList<Shape> shapes = 
+		ArrayList<Shape> shapes = (ArrayList<Shape>) Model.instance().getShapes();
+		
+		for(int i = 0; i < shapes.size(); i++) {
+			
+		}
 	}
 }
