0e27f48b713af7d0d9ed671ac333d396d8f7d9c8
crazyman9870 <crazyman9870@gmail.com>
2016-01-07 17:37:12 -0700

    Skeleton Implementation of the View

--- CS 355 src/src/cs355/model/drawing/Model.java
+++ CS 355 src/src/cs355/model/drawing/Model.java
@@ -3,15 +3,29 @@ package cs355.model.drawing;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Observer;
 
 public class Model extends CS355Drawing {
 
+	//Use a singleton so that the model can be accessed by the view when repainting
 	private static Model _instance;
+	
+	public int currentMode = -1;
+	private Color selectedColor;
+	private ArrayList<Shape> shapes;
+	private ArrayList<Observer> observers;
 
+	//If the model had not been initialized, it will be.
 	public static Model instance() {
 		if (_instance == null) 
 			_instance = new Model();
 		return _instance;
+	}
+	
+	private Model() {
+		selectedColor = Color.WHITE;
+		shapes = new ArrayList<Shape>();
+		observers = new ArrayList<Observer>();
 	}
 	
 	@Override
